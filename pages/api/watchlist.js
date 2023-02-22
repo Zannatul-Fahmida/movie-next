fa575ee9ea6b1e017dd2df1e06f58d6f823c8abf
zannatul <jannatulfahmida15@gmail.com>
2023-02-22 21:26:35 +0600

    watchlist remove added

--- pages/api/watchlist.js
+++ pages/api/watchlist.js
@@ -1,12 +1,12 @@
 import { connectToDatabase } from "../../lib/mongodb";
 import { getSession } from "next-auth/react";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
         const session = await getSession({ req });
     
         const { movieName, poster, releaseDate} = req.body;
-        console.log(movieName, poster, releaseDate);
 
         const client = await connectToDatabase();
     
@@ -22,6 +22,23 @@ export default async function handler(req, res) {
         });
     
         res.status(201).send({ message: "This movie is added successfully" });
+      } else if (req.method === "DELETE") {
+        const session = await getSession({ req });
+        const { id } = req.body;
+    
+        const client = await connectToDatabase();
+        const db = client.db();
+    
+        const result = await db.collection("watchlist").deleteOne({
+          _id: ObjectId(id),
+          email: session.user.email,
+        });
+    
+        if (result.deletedCount === 0) {
+          res.status(404).send({ message: "Movie not found in watchlist" });
+        } else {
+          res.status(200).send({ message: "Movie removed from watchlist" });
+        }
       } else {
         res.status(405).send({ message: "Method not allowed" });
       }
--- pages/dashboard/watchList/index.js
+++ pages/dashboard/watchList/index.js
@@ -2,12 +2,37 @@ import { connectToDatabase } from "../../../lib/mongodb";
 import DashboardLayout from "../DashboardLayout";
 import { getSession } from "next-auth/react";
 import Image from "next/image";
+import { toast, Toaster } from "react-hot-toast";
+import { useState } from "react";
 
-const WatchList = ({ lists }) => {
+const WatchList = ({ initialLists }) => {
   const imagePath = "https://image.tmdb.org/t/p/original";
+  const [lists, setLists] = useState(initialLists);
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch("/api/watchlist", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+
+      const data = await response.json();
+      toast.success("Movie removed from watchlist");
+
+      setLists(lists.filter((item) => item._id !== id));
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <DashboardLayout>
       <div className="p-10">
+        <Toaster />
         <h2 className="text-2xl font-bold text-rose-700 mb-2">
           My Watch Lists Are Here
         </h2>
@@ -27,10 +52,13 @@ const WatchList = ({ lists }) => {
                 <th scope="col" className="px-6 py-3">
                   Date & Time
                 </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
               </tr>
             </thead>
             <tbody>
-              {lists.map((list) => (
+              {lists?.map((list) => (
                 <tr
                   key={list._id}
                   className="bg-white border-b dark:bg-stone-800 dark:border-stone-700 hover:bg-gray-50 dark:hover:bg-stone-600"
@@ -48,6 +76,14 @@ const WatchList = ({ lists }) => {
                   <td className="px-6 py-4">{list.releaseDate}</td>
                   <td className="px-6 py-4">
                     {new Date(list.created).toLocaleString()}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      className="px-2 py-1 bg-red-600 text-white rounded shadow"
+                      onClick={() => handleDelete(list._id)}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -83,7 +119,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      lists: JSON.parse(JSON.stringify(lists)),
+      initialLists: JSON.parse(JSON.stringify(lists)),
     },
   };
 }
