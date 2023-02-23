5e1b5f79bd062aed7d411c81efbcd4452d421fce
zannatul <jannatulfahmida15@gmail.com>
2023-02-23 21:20:59 +0600

    reviews delete btn done

--- pages/api/review.js
+++ pages/api/review.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
@@ -34,6 +34,27 @@ export default async function handler(req, res) {
     });
 
     res.status(201).send({ message: "Review created successfully" });
+  } else if (req.method === "DELETE") {
+    const session = await getSession({ req });
+    const { id } = req.body;
+
+    const uri = process.env.MONGODB_URI;
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db();
+
+    const result = await db.collection("reviews").deleteOne({
+      _id: ObjectId(id),
+      email: session.user.email,
+    });
+
+    if (result.deletedCount === 0) {
+      res.status(404).send({ message: "Review not found" });
+    } else {
+      res.status(200).send({ message: "Review removed" });
+    }
   } else {
     res.status(405).send({ message: "Method not allowed" });
   }
--- pages/dashboard/myReviews/index.js
+++ pages/dashboard/myReviews/index.js
@@ -1,11 +1,35 @@
 import DashboardLayout from "../DashboardLayout";
 import { getSession } from "next-auth/react";
 import clientPromise from "../../../lib/mongodb";
+import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
-const MyReviews = ({ reviews }) => {
+const MyReviews = ({ initialReviews }) => {
+  const [reviews, setReviews] = useState(initialReviews);
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch("/api/review", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+
+      const data = await response.json();
+      toast.success("Review removed");
+
+      setReviews(reviews.filter((review) => review._id !== id));
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <DashboardLayout>
       <div className="p-10">
+        <Toaster />
         <h2 className="text-2xl font-bold text-rose-700 mb-2">
           My Reviews Are Here
         </h2>
@@ -25,6 +49,9 @@ const MyReviews = ({ reviews }) => {
                 <th scope="col" className="px-6 py-3">
                   Date & Time
                 </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -40,6 +67,14 @@ const MyReviews = ({ reviews }) => {
                   <td className="px-6 py-4">{review.rating}</td>
                   <td className="px-6 py-4">
                     {new Date(review.created).toLocaleString()}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      className="px-2 py-1 bg-red-600 text-white rounded shadow"
+                      onClick={() => handleDelete(review._id)}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -75,7 +110,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      reviews: JSON.parse(JSON.stringify(reviews)),
+      initialReviews: JSON.parse(JSON.stringify(reviews)),
     },
   };
 }
