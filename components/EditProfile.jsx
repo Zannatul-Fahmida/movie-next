c3de84777738ac8c4f78585ece15a281bd0c745c
zannatul <jannatulfahmida15@gmail.com>
2023-02-26 22:27:12 +0600

    updated

--- components/EditProfile.jsx
+++ components/EditProfile.jsx
@@ -87,8 +87,9 @@ const EditProfile = () => {
           className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-500 dark:bg-zinc-300 dark:text-black
           focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
           type="text"
-          {...register("name")}
+          {...register("name", { required: true })}
         />
+        {errors.name && <span>This field is required</span>}
         <input
           className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-500 dark:bg-zinc-300 dark:text-black
           focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
